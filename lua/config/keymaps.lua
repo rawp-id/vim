7968997f9e9d4337aad6bbecc0797e82909646da
Rifky Aryo <wpipit35@gmail.com>
2025-03-10 14:49:06 +0700

    update all
    go to beta!

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.opt.whichwrap:append("<,>,[,],h,l")
+
 vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>:call CocActionAsync('format')<CR>", { noremap = true, silent = true })
