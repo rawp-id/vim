13cd2e96dadfde96038beda78a4cbeb022aee8a5
Rifky Aryo <wpipit35@gmail.com>
2025-03-10 14:16:14 +0700

    update all
    update all go to beta!

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    config = function()
+      -- Tambahkan konfigurasi tambahan di sini, jika diperlukan
+    end
+  }
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -18,5 +18,42 @@ return {
         },
       },
     },
+
+    lazygit = {
+      -- Mengatur agar LazyGit menggunakan warna sesuai dengan colorscheme Neovim yang aktif
+      configure = true,
+
+      -- Konfigurasi tambahan untuk lazygit yang akan digabungkan dengan default
+      config = {
+        os = { editPreset = "nvim-remote" },
+        gui = {
+          -- Mengatur versi NerdFonts, set ke "" untuk menonaktifkan ikon
+          nerdFontsVersion = "3",
+        },
+      },
+
+      -- Tema untuk lazygit (menggunakan `snacks.lazygit.Config` untuk warna dan tampilan)
+      theme_path = vim.fn.stdpath("cache") .. "/lazygit-theme.yml", -- Path untuk tema lazygit
+      theme = {
+        [241]                      = { fg = "Special" },
+        activeBorderColor          = { fg = "MatchParen", bold = true },
+        cherryPickedCommitBgColor  = { fg = "Identifier" },
+        cherryPickedCommitFgColor  = { fg = "Function" },
+        defaultFgColor             = { fg = "Normal" },
+        inactiveBorderColor        = { fg = "FloatBorder" },
+        optionsTextColor           = { fg = "Function" },
+        searchingActiveBorderColor = { fg = "MatchParen", bold = true },
+        selectedLineBgColor        = { bg = "Visual" }, -- background colour for selected line
+        unstagedChangesColor       = { fg = "DiagnosticError" },
+      },
+
+      -- Pengaturan window untuk lazygit
+      win = {
+        style = "lazygit", -- Menggunakan gaya window default untuk lazygit
+      },
+
+      -- Argumen tambahan untuk perintah lazygit
+      args = nil, -- Kosongkan jika tidak memerlukan argumen tambahan, atau sesuaikan jika ada
+    }
   },
 }
