19c0c332199a27ebcc5ea8e7fe25bf12b590680c
Rifky Aryo <wpipit35@gmail.com>
2025-03-17 15:23:34 +0700

    update all

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -21,7 +21,7 @@ return {
       -- })
       -- Keymaps untuk komentar
       vim.keymap.set("n", "gcc", "<cmd>Commentary<CR>", { desc = "Toggle Comment line" })
-      vim.keymap.set("v", "gc", "gv<cmd>Commentary<CR>", { desc = "Toggle Comment selection" })
+      vim.keymap.set("v", "gc", "<Plug>Commentary", { desc = "Toggle Comment selection" })
       vim.keymap.set("n", "gco", "<cmd>Commentary<CR>o", { desc = "Add Comment Below" })
       vim.keymap.set("n", "gcO", "<cmd>Commentary<CR>O", { desc = "Add Comment Above" })
     end,
